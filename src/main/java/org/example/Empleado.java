7fed18aeb759c58acd4a690bb1ac64a962fddafd
alejoguzman0311@gmail.com <josea_1998@htomail.com>
2023-02-02 12:14:21 -0500

    se crean metodos en clase empleados

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Empleado {
+
+        // atributos
+        String nombre;
+
+    // constructor
+
+    public Empleado() {
+    }
+
+    //metodos especiales
+ public String saludar(String nombrecito){
+        String saludo="Hola "+nombrecito;
+        return saludo;
+    }
+
+    public int sumar(int numeroUno, int numeroDos){
+        int resultado = numeroUno + numeroDos;
+        return resultado;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,33 @@ public class Main {
         //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
         //EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
         //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
-        Invitado objetoDeClaseInvitado=new Invitado();
-        Invitado objeto2=new Invitado(333,true,"Juan",10000);
+
+       //para usar una clase creo un objeto
+        // para crear el objeto llamo la clase
+        //usar una clase es poder acceder a los atributos y metodos
+        //es el constructor el que crea el objeto
+
+        Empleado objeto = new Empleado();
+        //utilizar el objeto para acceder al atributo
+        objeto.nombre="pepe";
+
+        //utilizar el objeto para acceder a los metodos
+        int sumatoria = objeto.sumar(5,3);
+        String resultado=objeto.saludar("Daniel");
+
+        //salidas por consola
+        //acceder a mostrar el valor de un atributo
+        System.out.println(objeto.nombre);
+
+        //acceder a mostrar el retorno de la funcion
+        System.out.println(objeto.saludar("Daniel"));
+        System.out.println(resultado);
+        System.out.println(sumatoria);
 
 
-        //obtener el valor de los atributos
-        System.out.println(objetoDeClaseInvitado.nombre);
-        System.out.println(objeto2.nombre);
+
+
+
 
     }
 }
